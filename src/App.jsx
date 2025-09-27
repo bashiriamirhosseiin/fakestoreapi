d39f1ea1ec66d61235915d2d9ea4d2bf422236e1
bashiriamirhosseiin <amirhosseiinbashiri.fyo@gmail.com>
2025-09-27 09:51:13 -0700

    call api and save products to state

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,39 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function App() {
 
+  // Models
+  const [status, setStatus] = useState(0);
+  const [products, setProduct] = useState([]);
+
+  // api
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((res) => {
+        setProduct(res.data);
+        setStatus(res.status);
+        console.log("res: ", res);
+      })
+      .catch((err) => {
+        console.log("res: ", err);
+      });
+  }, []);
+
+  // model tracker
+  useEffect(() => {
+    console.log("products", products);
+  }, [products]);
+
+  useEffect(() => {
+    console.log("products", status);
+  }, [status]);
+
+  // jsx
   return (
-    <p>Hello World</p>
-  )
+    <div className="">
+      <p>status : {status}</p>
+    </div>
+  );
 }
