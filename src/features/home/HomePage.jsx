de44e882cf981fa59625add14ffa3c54fde1bea7
bashiriamirhosseiin <amirhosseiinbashiri.fyo@gmail.com>
2025-10-18 07:09:49 -0700

    request api and get all products

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -1,4 +1,37 @@
+// dep...
+import { useQuery } from "@tanstack/react-query"
+import api from "../../tools/api"
+
 export default function HomePage() {
+
+    async function queryFn() {
+        return await api.get("products");
+    }
+
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ["all-products"],
+        queryFn
+    })
+
+    if(isLoading){
+        return (
+            <div className="">
+                <p>Loading...</p>
+            </div>
+        )
+    }
+
+    if(isError) {
+        return (
+            <div className="">
+                <p>Error...</p>
+            </div>
+        )
+    }
+
+    console.log(data);
+    
+
     return (
         <div className="">
             <p>This is home page</p>
