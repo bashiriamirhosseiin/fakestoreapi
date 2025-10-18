8daf2b30ff73bd97841249bf197a4836c6cce38c
bashiriamirhosseiin <amirhosseiinbashiri.fyo@gmail.com>
2025-10-18 07:00:52 -0700

    create query functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@
 import { RouterProvider } from "react-router-dom"
 import router from "./router"
 
+// react query
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+const queryClient = new QueryClient();
+
+
 function App() {
 
   return (
     <>
-      <RouterProvider router={router}></RouterProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router}></RouterProvider>
+      </QueryClientProvider>
     </>
   )
 }
