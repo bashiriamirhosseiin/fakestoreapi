74a5a79d11fd8098872008c70a004224d5ff506d
bashiriamirhosseiin <amirhosseiinbashiri.fyo@gmail.com>
2025-10-18 06:33:34 -0700

    create api tpol

---
+++ src/tools/api.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+const api = axios.create({
+    baseURL: "https://fakestoreapi.com/"
+})
+
+api.interceptors.response.use(
+    (res) => res.data,
+    (err) => err
+)
+
+export default api
