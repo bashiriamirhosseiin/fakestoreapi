a983f9efa26abc64ab5289b859ad01102eb71f95
bashiriamirhosseiin <amirhosseiinbashiri.fyo@gmail.com>
2025-10-21 06:56:15 -0700

    connect cartbtn to zastand cart

--- src/components/CartButtons.jsx
+++ src/components/CartButtons.jsx
@@ -1,19 +1,27 @@
 import { useCallback, useEffect, useState } from "react"
+import useCart from "../store/useCart";
 
 export default function CartButtons({id}){
     
     const [status, setStatus] = useState(false);
+    const {cart, add, remove} = useCart();
+    
     useEffect(()=>{
-        // check item is in cart or not
-    }, [])
+        const foundItemIndex  = cart.findIndex((item)=> item.id == id);
+        if(foundItemIndex >= 0){
+            setStatus(true);
+        }else{
+            setStatus(false);
+        }
+    }, [cart])
 
     // functions...
     const handleRemoveFromCart = useCallback(()=>{
-
+        remove(id);
     }, [])
 
     const handleAddToCart = useCallback(()=>{
-
+        add(id);
     }, [])
 
 
