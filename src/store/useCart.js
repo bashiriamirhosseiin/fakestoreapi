fad32b287fbb28b6d4f78a42f87a39682cb48537
bashiriamirhosseiin <amirhosseiinbashiri.fyo@gmail.com>
2025-10-21 06:55:49 -0700

    create cart zastand

---
+++ src/store/useCart.js
@@ -0,0 +1,40 @@
+import { create } from 'zustand'
+
+const useCart = create((set)=>{
+    const prevCart = JSON.parse(localStorage.getItem("cart")) || [];
+    return {
+        cart: prevCart,
+        add: (id) => {
+            set((state) => {
+               return {cart: [...state.cart, {id: id, count: 1}]} 
+            })
+        },
+        remove: (id) => {
+            set((state) => {
+                const newCart = state.cart.filter(item => item.id != id)
+                return {cart: newCart}
+            })
+        },
+        clear: () => {
+            return {cart: []}
+        },
+        increase: (id) => {
+            set((state) => {
+                const newCart = state.cart.map((item)=>{
+                    return item.id == id ? {...item, count: item.count + 1} : item
+                })
+                return {cart: newCart}
+            })
+        },
+        decrease: (id) => {
+            set((state) => {
+                const newCart = state.cart.map((item) => {
+                    return item.id == id ? {...item, count: Math.max(1, item.count-1)} : item
+                })
+                return {cart: newCart}
+            })
+        }
+    }
+})
+
+export default useCart
