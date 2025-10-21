6ef0da6b09439dec744df2f2b9e87f9248c4c9a4
bashiriamirhosseiin <amirhosseiinbashiri.fyo@gmail.com>
2025-10-21 14:50:13 -0700

    create cart structure

--- src/features/cart/CartPage.jsx
+++ src/features/cart/CartPage.jsx
@@ -1,6 +1,71 @@
-export default function CartPage () {
+// dep...
+import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import api from "../../tools/api";
 
+// components
+import CartItem from "./components/CartItem";
+
+// store
+import useCart from "../../store/useCart";
+
+export default function CartPage() {
+
+  const [showItems, setShowItems] = useState([]);
+  const {cart, clear} = useCart();
+
+  async function queryFn() {
+    return await api.get("products");
+  }
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["all-products"],
+    queryFn,
+  });
+
+  useEffect(()=>{
+    if(!isLoading && data) {
+        const items = cart.map((item)=>{
+          const product = data.find(p => p.id == item.id)
+          return {
+            ...product, 
+            count: item.count
+          }
+        })
+        setShowItems(items);
+    }
+  }, [data, cart])
+
+  
+
+  if (isLoading) {
     return (
-        <div className=""></div>
-    )
+      <div className="">
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className="">
+        <p>Error...</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="">
+      {showItems.map((item, index) => (
+        <CartItem 
+          key={index}   
+          id={item.id}
+          title={item.title}  
+          count={item.count}
+        />
+      ))}
+      <hr />
+      <button onClick={()=>{clear()}}>Clear</button>
+    </div>
+  )
 }
--- src/features/cart/components/CartItem.jsx
+++ src/features/cart/components/CartItem.jsx
@@ -1,6 +1,16 @@
-export default function CartItem () {
+// store
+import useCart from "../../../store/useCart"
+
+export default function CartItem ({id, title, count}) {
+    const {remove, increase, decrease} = useCart()
 
     return (
-        <div className=""></div>
+        <div className="">
+            <p>{title}</p>
+            <p>count: {count}</p>
+            <p><button onClick={()=>{remove(id)}}>Delete</button></p>
+            <p><button onClick={()=>{increase(id)}}>Increase</button></p>
+            <p><button onClick={()=>{decrease(id)}}>Decrease</button></p>
+        </div>
     )
 }
