f64a3e95d2a6e80d38febe96464072be64349e17
bashiriamirhosseiin <amirhosseiinbashiri.fyo@gmail.com>
2025-10-21 15:56:35 -0700

    create sec header (product header) and design it

--- src/components/header/SecHeader.jsx
+++ src/components/header/SecHeader.jsx
@@ -1,7 +1,36 @@
-export default function SecHeader() {
+// dep
+import { Link, useNavigate } from "react-router-dom";
+import { useCallback, useEffect, useState } from "react";
+
+// icons
+import { FaAngleLeft } from "react-icons/fa6";
+import { IoHeartOutline } from "react-icons/io5";
+import { GoHeartFill } from "react-icons/go";
+
+// store
+import useWishlist from "../../store/useWishlist";
+
+
+export default function SecHeader ({id}) {
+
+    const [like, setLike] = useState(false)
+    const {wishlist, add, remove} = useWishlist()
+    
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        setLike(wishlist.includes(id))
+    }, [wishlist])
+
+    const backHandler = useCallback(() => {
+        navigate(-1);
+    }, [])
+
     return (
-        <div className="">
-            sec header
+        <div className="w-full flex justify-between items-center px-[6.4%] py-5 shadow-md mb-3">
+           <FaAngleLeft size={19} onClick={backHandler} className="cursor-pointer"/>
+            <p className="text-lg font-medium">Detail</p>
+            { like ? <GoHeartFill size={19} color="#930000" onClick={()=>{remove(id)}} className="cursor-pointer" /> : <IoHeartOutline size={19} onClick={()=>{add(id)}} className="cursor-pointer" /> }
         </div>
     )
 }
--- src/features/singleProduct/SingleProduct.jsx
+++ src/features/singleProduct/SingleProduct.jsx
@@ -1,48 +1,55 @@
 // dep...
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import api from "../../tools/api";
 import { useQuery } from "@tanstack/react-query";
 import CartButtons from "../../components/CartButtons";
+import SecHeader from "../../components/header/SecHeader";
 
-export default function SingleProduct(){
+export default function SingleProduct() {
+  // get id param:
+  const { id } = useParams();
 
-    // get id param:
-    const {id} = useParams();
-    
-    // api get data:
-    async function queryFn(){
-        return await api.get(`products/${id}`)
-    }
+  // api get data:
+  async function queryFn() {
+    return await api.get(`products/${id}`);
+  }
 
-    const {data, isLoading, isError} = useQuery({
-        queryKey: [`single-product-${id}`],
-        queryFn
-    })
+  const { data, isLoading, isError } = useQuery({
+    queryKey: [`single-product-${id}`],
+    queryFn,
+  });
 
-    if(isLoading){
-        return (
-            <div className="">
-                <p>Loading...</p>
-            </div>
-        )
-    }
-
-    if(isError) {
-        return (
-            <div className="">
-                <p>Error...</p>
-            </div>
-        )
-    }
-    
+  if (isLoading) {
     return (
-        <div className="">
-            <img src={data.image} alt="" />
-            <p>{data.title}</p>
-            <p>{data.description}</p>
-            <p>{data.price}$</p>
-            <p>{data.rating.rate}({data.rating.count})</p>
-            <CartButtons id={data.id}/>
-        </div>
-    )
+      <div className="">
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className="">
+        <p>Error...</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col">
+      <SecHeader 
+        id={data.id}
+      />
+      <div className="grow-1">
+        <img src={data.image} alt="" />
+        <p>{data.title}</p>
+        <p>{data.description}</p>
+        <p>{data.price}$</p>
+        <p>
+          {data.rating.rate}({data.rating.count})
+        </p>
+        <CartButtons id={data.id} />
+      </div>
+    </div>
+  );
 }
