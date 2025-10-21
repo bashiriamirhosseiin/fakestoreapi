86c59fbc0841a1755c08e5b959e18d6904cf250b
bashiriamirhosseiin <amirhosseiinbashiri.fyo@gmail.com>
2025-10-21 06:57:49 -0700

    set a listener to cart for log and localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+// dep...
+import { useEffect } from "react";
+
 // router
 import { RouterProvider } from "react-router-dom"
 import router from "./router"
@@ -6,8 +9,18 @@ import router from "./router"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 const queryClient = new QueryClient();
 
+// store
+import useCart from "./store/useCart";
+
 
 function App() {
+
+  const {cart} = useCart()
+
+  useEffect(()=>{
+    console.log(cart);
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }, [cart])
 
   return (
     <>
