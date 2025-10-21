42d66f25218a5fd13ea87ea8692518aa67c314eb
bashiriamirhosseiin <amirhosseiinbashiri.fyo@gmail.com>
2025-10-21 15:44:59 -0700

    create and style main page search bar

---
+++ src/components/header/Search.jsx
@@ -0,0 +1,33 @@
+// dep
+import { useState } from "react";
+
+// icons
+import { RiResetLeftFill } from "react-icons/ri";
+
+export default function Search() {
+  const [search, setSearch] = useState("");
+
+  return (
+    <div className="flex justify-between items-center gap-4">
+      <div className="rounded-2xl bg-[#313131] h-[52px] w-full px-3">
+        <input
+          type="text"
+          placeholder="Search Item"
+          className="text-gray-100 h-full w-full bg-none outline-0 border-0"
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+          value={search}
+        />
+      </div>
+      <div
+        onClick={() => {
+          setSearch("");
+        }}
+        className="shrink-0 w-[52px] h-[52px] bg-[#C67C4E] rounded-2xl flex justify-center items-center cursor-pointer"
+      >
+        <RiResetLeftFill color="white" size={24} />
+      </div>
+    </div>
+  );
+}
