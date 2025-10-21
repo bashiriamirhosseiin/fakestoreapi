fd566313d777edf85f6ddd08061946dc972918a0
bashiriamirhosseiin <amirhosseiinbashiri.fyo@gmail.com>
2025-10-21 15:43:57 -0700

    style main header

--- src/components/header/MainHeader.jsx
+++ src/components/header/MainHeader.jsx
@@ -1,7 +1,12 @@
+// components
+import Search from "./Search";
+
 export default function MainHeader() {
     return (
-        <div className="">
-            Main Header
+        <div className="w-full pt-[40px] px-[25px] pb-[24px] bg-header">
+            <p className="text-xs text-[#a2a2a2] mb-2">Welcome</p>
+            <p className="text-[14px] text-[#d8d8d8] flex items-center gap-1 mb-[24px]">Lorem ipsum</p>
+            <Search />
         </div>
     )
 }
