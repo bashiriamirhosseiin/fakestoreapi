ca3032b1efdbb66ee8cdfe97d48e3cdbc3dc8b7d
bashiriamirhosseiin <amirhosseiinbashiri.fyo@gmail.com>
2025-10-21 16:18:33 -0700

    add pointer to product

--- src/features/home/components/ProductItem.jsx
+++ src/features/home/components/ProductItem.jsx
@@ -5,9 +5,17 @@ import { useNavigate } from "react-router-dom"
 import CartButtons from "../../../components/CartButtons"
 import WishlistBtn from "../../../components/WishlistBtn"
 
+// icons
+import { LuPlus } from "react-icons/lu";
+import { TiStarFullOutline } from "react-icons/ti";
+import { BsX } from "react-icons/bs";
+
+
+
 export default function ProductItem({
     id, 
     title,
+    category,
     description,
     image,
     price,
@@ -18,14 +26,20 @@ export default function ProductItem({
     const navigate = useNavigate()
 
     return (
-        <div className="" onClick={()=>{navigate(`product/${id}`)}}>
-            <img src={image} alt="" />
-            <p>{title}</p>
-            <p>{description}</p>
-            <p>{rate}({rateCount})</p>
-            <p>{price}$</p>
-            <CartButtons id={id} />
-            <WishlistBtn id={id} />
-        </div>
+        <div className="cursor-pointer w-[140px] p-2 flex flex-col" onClick={()=>{navigate(`product/${id}`)}}>
+              <div className="relative w-full h-[130px] rounded-2xl overflow-clip p-2 mb-2 shadow-md">
+                <img className="object-contain object-center" src={image} alt="" />
+                <div className="absolute top-0 right-0 py-1 px-3 flex gap-1 items-center text-xs bg-[#92542e] text-white rounded-bl-2xl">
+                  <TiStarFullOutline color="#FBBE21" size={11} />
+                  {rate}
+                </div>
+              </div>
+              <p className="font-semibold text-md mb-1">{title.substr(0, 9)}</p>
+              <p className="text-xs text-gray-500 mb-2">{category.substr(0, 9)}</p>
+              <div className="w-full flex justify-between items-center">
+                <p className="text-[18px] font-semibold">${price}</p>
+                <CartButtons id={id} />
+              </div>
+            </div>
     )
 }
