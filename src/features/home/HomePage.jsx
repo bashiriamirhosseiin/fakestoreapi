07265d6b8551fb74de48a11eca23dd1c562effc9
bashiriamirhosseiin <amirhosseiinbashiri.fyo@gmail.com>
2025-10-21 17:30:10 -0700

    show  product items in home

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -1,6 +1,6 @@
 // dep...
-import { useQuery } from "@tanstack/react-query"
-import api from "../../tools/api"
+import { useQuery } from "@tanstack/react-query";
+import api from "../../tools/api";
 
 // components
 import ProductItem from "./components/ProductItem";
@@ -8,55 +8,54 @@ import MainHeader from "../../components/header/MainHeader";
 import Footer from "../../components/Footer";
 
 export default function HomePage() {
+  async function queryFn() {
+    return await api.get("products");
+  }
 
-    async function queryFn() {
-        return await api.get("products");
-    }
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["all-products"],
+    queryFn,
+  });
 
-    const { data, isLoading, isError } = useQuery({
-        queryKey: ["all-products"],
-        queryFn
-    })
-
-    if(isLoading){
-        return (
-            <div className="">
-                <p>Loading...</p>
-            </div>
-        )
-    }
-
-    if(isError) {
-        return (
-            <div className="">
-                <p>Error...</p>
-            </div>
-        )
-    }
-
+  if (isLoading) {
     return (
-        <div className="h-full flex flex-col">
-            <div className="w-full">
-                <MainHeader />
-            </div>
-            <div className="w-full grow-1 h-full flex flex-col justify-between">
-                <div className="w-full grow-1 bg-green-500">content</div>
-                <Footer />
-            </div>
-          
-            {/* {data.map((item, index)=>(
-                <ProductItem 
-                    key={index}
-                    id={item.id} 
-                    title={item.title} 
-                    description={item.description} 
-                    image={item.image} 
-                    price={item.price} 
-                    rate={item.rating.rate} 
-                    rateCount={item.rating.count} 
-                />
-            ))} */}
-            
+      <div className="">
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className="">
+        <p>Error...</p>
+      </div>
+    );
+  }
+
+ return (
+    <div className="h-full grid grid-rows-[auto_1fr_auto]">
+      <div className="w-full">
+        <MainHeader />
+      </div>
+      <div className="overflow-auto pt-4">
+        <div className="w-full px-[25px] flex gap-[15px] flex-wrap justify-between">
+          {data.map((item, index) => (
+            <ProductItem
+              key={index}
+              id={item.id}
+              category={item.category}
+              title={item.title}
+              description={item.description}
+              image={item.image}
+              price={item.price}
+              rate={item.rating.rate}
+              rateCount={item.rating.count}
+            />
+          ))}
         </div>
-    )
+      </div>
+      <Footer />
+    </div>
+  );
 }
